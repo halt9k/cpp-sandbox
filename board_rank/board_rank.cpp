732f319dd94605c952972d1c19b59413df0c8d08
halt9k <49090368+halt9k@users.noreply.github.com>
2023-08-07 20:50:11 +0300

    wip: ranking challenge, clean short solution

--- board_rank/board_rank.cpp
+++ board_rank/board_rank.cpp
@@ -18,65 +18,47 @@ using namespace Str;
     The function is expected to return an INTEGER_ARRAY.
     The function accepts following parameters:
     1. INTEGER_ARRAY prev_scores
-    2. INTEGER_ARRAY new_player_scores
+    2. INTEGER_ARRAY new_scores
 */
 
+// prev_scores         100 100 50 40 40 20 10  idp->
+// prev_positions      1   1   2  3  3  4  5
 
-int get_score_position(vector<int> prev_scores, vector<int> prev_positions, int player_score, int& below_idx)
+// new_scores          5 25 50 120             idn<-
+// new_ranks       ? ?  ?  ?
+
+vector<int> climbingLeaderboard(vector<int> prev_scores, vector<int> new_scores)
 	{
-	// prev_scores      100 100 50 40 40 20 10
-	// prev_positions   1   1   2  3  3  4  5
+	vector<int> new_ranks(new_scores.size());
 	
-	for (int i = below_idx; i >= 0; i--)
+	int idp = 0, idn = new_scores.size() - 1, idp_rank = 1;
+	int max_cycles = prev_scores.size() + new_scores.size();
+	
+	for (int i = 0; i < max_cycles; i++)
 		{
-		if (player_score < prev_scores[i])
+		if (new_scores[idn] >= prev_scores[idp])
 			{
-			below_idx = i;
-			return prev_positions[below_idx] + 1;
+			new_ranks[idn] = idp_rank;
+			idn--;
 			}
+		else if (idp < prev_scores.size() - 1)
+			{
+			idp++;
+			
+			if (prev_scores[idp] < prev_scores[idp - 1])
+				idp_rank++;
+			}
+		else // score below lowest
+			{
+			new_ranks[idn] = idp_rank + 1;
+			idn--;
+			}
+			
+		if (idn < 0)
+			break;
 		}
 		
-	return 1;
-	}
-
-
-vector<int> estimate_prev_positions(vector<int> ranked)
-	{
-	vector<int> rank_positions(ranked.size());
-	
-	int cur_rank = 1;
-	int prev_score = ranked[0];
-	
-	for (int i = 0; i < ranked.size(); ++i)
-		{
-		int v = ranked[i];
-		if (v < prev_score)
-			{
-			prev_score = v;
-			cur_rank++;
-			}
-		rank_positions[i] = cur_rank;
-		}
-		
-	return rank_positions;
-	}
-
-vector<int> climbingLeaderboard(vector<int> prev_scores, vector<int> new_player_scores)
-	{
-	// prev_scores           100 100 50 40 40 20 10  idx_r<-
-	// prev_positions        1   1   2  3  3  4  5
-	
-	// new_player_scores           5 25 50 120             idx_p->
-	// player_positions            ? ?  ?  ?
-	
-	vector<int>& prev_positions = estimate_prev_positions(prev_scores);
-	
-	int below_idx = prev_scores.size() - 1;
-	vector<int> player_positions(new_player_scores.size());
-	for (int i = 0; i < new_player_scores.size(); ++i)
-		player_positions[i] = get_score_position(prev_scores, prev_positions, new_player_scores[i], below_idx);
-		
-	return player_positions;
+	return new_ranks;
 	}
 
 
@@ -156,45 +138,3 @@ int main()
 	
 	return 0;
 	}
-
-
-/*
-    int main_magic_square()
-    {
-    // ofstream fout("./out/out.txt");
-    ofstream fout;
-    ifstream fin("./in/test_msquare1.txt");
-
-    vector<vector<int>> s(3);
-
-    for (int i = 0; i < 3; i++)
-        {
-        s[i].resize(3);
-
-        string s_row_temp_temp;
-        getline(fin, s_row_temp_temp);
-
-        vector<string> s_row_temp = Str::split(Str::rtrim(s_row_temp_temp));
-
-        for (int j = 0; j < 3; j++)
-            {
-            int s_row_item = stoi(s_row_temp[j]);
-
-            s[i][j] = s_row_item;
-            }
-        }
-
-    string s_correct_answer;
-    getline(fin, s_correct_answer);
-    int result = formingMagicSquare(s);
-
-    cout << "Correct: " << s_correct_answer << endl;
-    cout << result << "\n";
-    fout << result << "\n";
-
-    fout.close();
-    fin.close();
-
-    return getwchar();
-    }
-*/
