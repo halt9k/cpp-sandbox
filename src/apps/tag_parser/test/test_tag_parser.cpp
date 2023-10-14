0c72953ce937f83abcb759809d003a8c0a2294cb
halt9k <49090368+halt9k@users.noreply.github.com>
2023-10-14 17:48:12 +0300

    test: works as expected

--- src/apps/tag_parser/test/test_tag_parser.cpp
+++ src/apps/tag_parser/test/test_tag_parser.cpp
@@ -1,20 +1,9 @@
-// #define CATCH_CONFIG_MAIN
-// #include <catch.hpp>
-
 #include "pch.h"
 
 #include "../tag_parser.h"
 
-// TEST_CASE("set_duplicates", "[util]")
 
-TEST(TestCaseName1, TestName1)
-	{
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-	}
-
-
-TEST(set_duplicates_1, util_1)
+TEST(tag_parser, fixtures_in_out)
 	{
 	tag_parser();
 	EXPECT_TRUE(true);
--- src/common/sandbox.cpp
+++ src/common/sandbox.cpp
@@ -1,9 +1,16 @@
-#include "sandbox.h"
+// #include "sandbox.h"
+// sandbox.h has any main() are auto-renamed
+// #undef main
 
-// one main() is desired, other mains are auto-renamed
-#undef main
+
+#include "..\apps\tag_parser\tag_parser.h"
+
+
 int main()
 	{
+	// an app to run by default
+	tag_parser();
+	
 	return 0;
 	};
 
