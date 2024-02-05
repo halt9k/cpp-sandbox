ff5267e78f84570d727586cfe7ea7cdc5c894ef5
halt9k <49090368+halt9k@users.noreply.github.com>
2024-02-05 09:54:45 +0300

    wip: tag_parser refactor,edge cases

--- src/apps/tag_parser/tag_parser.cpp
+++ src/apps/tag_parser/tag_parser.cpp
@@ -4,25 +4,87 @@
 #include "sandbox_adapter.h"
 
 using DictStr = std::map<string, string>;
+using string_view = std::string_view;
 
 
-void parse(string& line, DictStr& dict_str, string& cur_path)
+inline void push_and_erase_word(VStrs& words, string& word)
 	{
-	assert(line.size() > 2);
-	VStrs words = Str::split(line, " =");
-	assert(words.size() > 0);
-	
-	string& tag = words.front();
-	string& w_last = words.back();
-	
-	Str::safe_erase(w_last, -1, ">");
-	Str::safe_erase(tag, 0, "<");
-	
-	bool is_closing_tag = (tag[0] == '/');
+	words.push_back(word);
+	word.clear();
+	}
+
+
+inline VStrs get_words_and_quotes(std::string_view& line)
+	{
+	assert(line.size() > 0);
+
+	VStrs words;
+	string next_word = "";
+	bool in_quote = false;
+	char prev_c = '\n';
+
+	for (const char& c : line)
+		{
+		if (c == '"')
+			{
+			in_quote = !in_quote;
+			if (!in_quote)
+				push_and_erase_word(words, next_word);
+			continue;
+			}
+
+		if (in_quote)
+			{
+			next_word.push_back(c);
+			continue;
+			}
+
+		assert(!in_quote);
+
+		// in word
+		if (c != ' ')
+			{
+			next_word.push_back(c);
+			continue;
+			}
+
+		// word end cases
+		if (c == '=' ||
+			(c == ' ' && next_word.size() > 0))
+			push_and_erase_word(words, next_word);
+
+		prev_c = c;
+		}
+
+	if (next_word.size() > 0)
+		push_and_erase_word(words, next_word);
+
+	assert(!in_quote);
+	return words;
+	}
+
+
+void parse(string& braced_line, DictStr& dict_str, string& cur_path)
+	{
+	// strip braces
+	assert(braced_line.front() == '<' && braced_line.back() == '>');
+	string_view line(braced_line.data() + 1, braced_line.size() - 2);
+
+	VStrs words = get_words_and_quotes(line);
+	assert(words.size() > 0 && (words.size() - 1) % 3 == 0);
+
+	string& full_tag = words.front();
+	assert(full_tag.size() > 0);
+	bool is_closing_tag = (full_tag[0] == '/');
+	string_view tag;
+	if (is_closing_tag) 
+		tag = string_view(full_tag.data() + 1);
+	else
+		tag = string_view(full_tag);
+
 	if (is_closing_tag)
 		{
-		Str::safe_erase(tag, 0, "/");
-		Str::safe_erase(cur_path, cur_path.size() - tag.size(), tag);
+		Str::safe_erase(cur_path, cur_path.size() - tag.size(), tag.data());
 		if (cur_path.size() > 1)
 			Str::safe_erase(cur_path, -1, ".");
 		return;
@@ -34,52 +96,46 @@ void parse(string& line, DictStr& dict_str, string& cur_path)
 		cur_path += tag;
 		words.erase(words.begin());
 		}
-		
-	while (words.size() > 0)
+
+	assert(words.size() % 3 == 0);
+	int eqs = words.size() / 3;
+	FOR(eq, 0, eqs - 1)
 		{
-		assert(words.size() % 2 == 0);
-		string attr = words[0];
-		string attr_val = words[1];
-		
-		Str::safe_erase(attr_val, 0, "\"");
-		Str::safe_erase(attr_val, -1, "\"");
-		
+		string attr = words[eq * 3];
+		string attr_val = words[eq * 3 + 2];
 		dict_str[cur_path + '~' + attr] = attr_val;
-		
-		words.erase(words.begin(), words.begin() + 2);
 		}
-		
 	}
 
 
 void tag_parser()
 	{
 	VStrs lines = FIO::cin_read_lines();
-	
+
 	VInts inputs = Vec::str_to_int(Str::split(lines[0]));
 	int N = inputs[0];
 	int Q = inputs[1];
-	
+
 	// cout << N << Q << endl;
-	
+
 	DictStr dict_str;
 	string cur_path = "";
-	FOR(x, 1, N)
+	FOR(x, 1, N - 1)
 		{
 		parse(lines[x], dict_str, cur_path);
 		}
-		
+
 	VStrs out_lines;
 	FOR(x, N + 1, N + Q)
 		{
 		string req = lines[x];
-		
+
 		if (dict_str.count(req) > 0)
 			out_lines.push_back(dict_str[req]);
 		else
 			out_lines.push_back("Not Found!");
 		}
-		
+
 	FIO::cout_output_lines(out_lines);
 	}
 
